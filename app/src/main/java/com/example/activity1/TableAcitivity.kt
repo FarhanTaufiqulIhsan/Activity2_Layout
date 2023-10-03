3beb21ad15210d89e72926b39e4db492028cd92d
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-10-03 23:16:35 +0700

    Creating Table layout

---
+++ app/src/main/java/com/example/activity1/TableAcitivity.kt
@@ -0,0 +1,11 @@
+package com.example.activity1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TableAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table)
+    }
+}
